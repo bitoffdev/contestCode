913a9f622b6b553b5bd8cd0e0aad9cb943493f05
elliospizzaman <elliospizzaman@users.noreply.github.com>
2013-11-10 16:22:25 -0500

    Create qualificationA.py

---
+++ Google-Code-Jam-2012/qualificationA.py
@@ -0,0 +1,14 @@
+from sys import argv
+script, infile, outfile = argv
+data = [line.rstrip('\n') for line in open(infile)]
+data.pop(0)
+english= " abcdefghijklmnopqrstuvwxyz"
+google = " ynficwlbkuomxsevzpdrjgthaq"
+i = 1
+output = open(outfile,"w")
+for item in data:
+        tempstr = ""
+        for letter in item:
+                tempstr+=english[google.find(letter)]
+        output.write("Case #%d: %s\n" %(i, tempstr))
+        i+=1
